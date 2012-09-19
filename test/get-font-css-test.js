e8afa5caf69d60bda96d1e52580a02dccd721ce0
Shane Tomlinson <stomlinson@mozilla.com>
2012-09-19 14:32:50 +0100

    Add more tests, rename setup options.
    setup option changes:
    * languageToLocations=>language_to_locations
    * urlModifier=>url_modifier
    
    add tests for:
    * default undefined location is "extended"
    * default missing location is "extended" if map specific location is unavailable.
    * proper handling of url_modifier

--- test/get-font-css-test.js
+++ test/get-font-css-test.js
@@ -17,41 +17,75 @@ function getLanguageToLocationsConfig() {
   return loadJSON(__dirname + "/sample-config/language-to-location.json");
 }
 
-function testFormatDeclared(test, ua, types) {
+function testCSSContains(test, ua, lang, types) {
   var css = css_generator.get_font_css({
     ua: ua,
-    lang: "en",
+    lang: lang,
     fonts: ["OpenSansRegular"]
   });
-
-  console.log(css);
 
   types.forEach(function(type) {
     test.notEqual(css.indexOf(type), -1, type + " found");
   });
 }
 
-exports.setup = nodeunit.testCase({
+exports.test_css_generated = nodeunit.testCase({
   setUp: function (cb) {
     css_generator.setup({
       fonts: getFontConfig(),
-      languageToLocations: getLanguageToLocationsConfig()
+      language_to_locations: getLanguageToLocationsConfig(),
+      url_modifier: function(url) { return "/inserted_sha" + url; }
     });
     cb();
   },
   tearDown: function (cb) {
     cb();
   },
-  "get correct fonts for browser": function(test) {
-    testFormatDeclared(test, "Firefox", ["local", "woff"]);
-    testFormatDeclared(test, "Chrome", ["local", "woff"]);
-    testFormatDeclared(test, "Safari", ["local", "woff"]);
-    testFormatDeclared(test, "MSIE 8.0", ["local", "eot"]);
-    testFormatDeclared(test, "MSIE 9.0", ["local", "eot"]);
-    testFormatDeclared(test, "Opera", ["local", "eot"]);
-    testFormatDeclared(test, "iOS", ["local", "ttf"]);
 
+  "en maps to latin": function(test) {
+    testCSSContains(test, "Firefox", "en", ["/inserted_sha/fonts/OpenSans-Regular-latin.woff"]);
+    test.done();
+  },
+
+  "it-ch maps to extended": function(test) {
+    testCSSContains(test, "Firefox", "it-ch", ["/inserted_sha/fonts/OpenSans-Regular-extended.woff"]);
+    test.done();
+  },
+
+  "ru maps to cyrillic": function(test) {
+    testCSSContains(test, "Firefox", "ru", ["/inserted_sha/fonts/OpenSans-Regular-cyrillic.woff"]);
+    test.done();
+  },
+
+  "unknown languages default to the extended font set": function(test) {
+    testCSSContains(test, "Firefox", "cz", ["/inserted_sha/fonts/OpenSans-Regular-extended.woff"]);
+    test.done();
+  },
+
+  "missing font location falls back to extended": function(test) {
+    // jp should use japanese which has no location defined
+    testCSSContains(test, "Firefox", "jp", ["/inserted_sha/fonts/OpenSans-Regular-extended.woff"]);
+    test.done();
+  },
+
+  "Firefox, Chrome and Safari all support local and .woff files": function(test) {
+    testCSSContains(test, "Firefox", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular-latin.woff"]);
+    testCSSContains(test, "Chrome", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular-latin.woff"]);
+    testCSSContains(test, "Safari", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular-latin.woff"]);
+    test.done();
+  },
+
+  "IE and Opera support local and .eot files": function(test) {
+    testCSSContains(test, "MSIE 8.0", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular.eot"]);
+    testCSSContains(test, "MSIE 9.0", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular.eot"]);
+    testCSSContains(test, "Opera", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular.eot"]);
+    test.done();
+  },
+
+
+  "iOS supports local and ttf files": function(test) {
+    testCSSContains(test, "iOS", "en", ["local", "/inserted_sha/fonts/OpenSans-Regular-latin.ttf"]);
     test.done();
   }
-});
 
+});
